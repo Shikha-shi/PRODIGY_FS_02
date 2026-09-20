a7a3ee60122aae97a9d00024f2965a7ab5caf0bb
Shikha Yadav <2k24.cs1o.2413211@gmail.com>
2026-09-20 15:29:59 +0530

    feat:Configure PostgresSQL database connection

---
+++ backend/app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import sessionmaker,declarative_base
+from app.settings import settings
+
+Base=declarative_base()
+engine=create_engine(
+    (settings.DB_CONNECTION)
+)
+
+sessionLocal=sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
